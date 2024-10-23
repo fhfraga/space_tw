c2fdfc1fef25718b56a3af0e66ed58aff04b4951
Fernando <f.henrique.fraga@gmail.com>
2024-10-23 08:55:44 -0300

    Adição de registro de usuários para criação de novos usuários que podem utilizar as funcionalidades do projeto

--- store/forms.py
+++ store/forms.py
@@ -1,12 +1,20 @@
-from datetime import timedelta
-
 from django import forms
-from django.forms.widgets import DateInput
-from django.utils import timezone
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Product
 
 
+# Formulário para registro de usuários
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+# Formulário para cadastro de salas
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
@@ -20,47 +28,3 @@ class ProductForm(forms.ModelForm):
             "available_from",
             "available_to",
         ]
-        labels = {
-            "name": "Nome",
-            "price": "Preço",
-            "space_type": "Tipo de Espaço",
-            "features": "Características",
-            "image": "Imagem",
-            "on_sale": "Em Promoção",
-            "available_from": "Disponível a Partir de",
-            "available_to": "Disponível Até",
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(ProductForm, self).__init__(*args, **kwargs)
-
-        # Define a data padrão para available_from e available_to
-        # if "available_from" not in self.data and not self.instance.pk:
-        #     self.fields["available_from"].initial = timezone.now().date()  # Data atual
-        #     self.fields["available_to"].initial = timezone.now().date() + timedelta(
-        #         days=3650
-        #     )  # 10 anos à frente
-
-        # Configurar widgets para formatar datas
-        self.fields["available_from"].widget = DateInput(
-            format="%d/%m/%Y",
-            attrs={
-                "type": "date",
-                "min": str(timezone.now().date()),
-                "max": str((timezone.now() + timedelta(days=365)).date()),
-            },
-        )
-        self.fields["available_to"].widget = DateInput(
-            format="%d/%m/%Y",
-            attrs={
-                "type": "date",
-                "min": str((timezone.now() + timedelta(days=1)).date()),
-                "max": str((timezone.now() + timedelta(days=365)).date()),
-            },
-        )
-
-    def clean_features(self):
-        features = self.cleaned_data.get("features")
-        if not features:
-            return "Nenhuma"
-        return features
