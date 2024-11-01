fa822a35b7a8befd5186ebf80215d2b2feb16a2c
JeandersonAmaral <jeanderson.amaral@outlook.com>
2024-11-01 13:53:06 -0300

    Menu responsivo criado

--- static/js/menubar.js
+++ static/js/menubar.js
@@ -1,17 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('auth-menu-toggle');
-    const menu = document.getElementById('auth-menu');
+    // Controle do Menu Desktop
+    const toggleButtonDesktop = document.getElementById('auth-menu-toggle-desktop');
+    const menuDesktop = document.getElementById('auth-menu-desktop');
+    
+    if (toggleButtonDesktop && menuDesktop) {
+        toggleButtonDesktop.onclick = function(event) {
+            event.stopPropagation(); 
+            menuDesktop.classList.toggle('hidden');
+        };
+    }
 
-    toggleButton.onclick = function(event) {
-        event.stopPropagation(); 
-        menu.classList.toggle('hidden');
-    };
+    // Controle do Menu Mobile
+    const toggleButtonMobile = document.getElementById('auth-menu-toggle-mobile');
+    const menuMobile = document.getElementById('auth-menu-mobile');
+    
+    if (toggleButtonMobile && menuMobile) {
+        toggleButtonMobile.onclick = function(event) {
+            event.stopPropagation();
+            menuMobile.classList.toggle('hidden');
+        };
+    }
 
+    // Fecha os menus ao clicar fora deles
     window.onclick = function(event) {
-        if (!event.target.matches('#auth-menu-toggle') && !menu.contains(event.target)) {
-            if (!menu.classList.contains('hidden')) {
-                menu.classList.add('hidden');
-            }
+        // Fecha o menu desktop se o clique estiver fora
+        if (!event.target.matches('#auth-menu-toggle-desktop') && menuDesktop && !menuDesktop.contains(event.target)) {
+            menuDesktop.classList.add('hidden');
+        }
+
+        // Fecha o menu mobile se o clique estiver fora
+        if (!event.target.matches('#auth-menu-toggle-mobile') && menuMobile && !menuMobile.contains(event.target)) {
+            menuMobile.classList.add('hidden');
         }
     };
 });
